3b7318bcd757f137b7a80ee10ddcfd12f600b79d
asperand <jamesdavis0901@gmail.com>
2025-01-28 11:21:14 -0500

    Roslyn working correctly now

--- init.lua
+++ init.lua
@@ -347,9 +347,6 @@ require('lazy').setup({
   },
 
   {
-    'jiangmiao/auto-pairs',
-  },
-  {
     'seblj/roslyn.nvim',
     ft = 'cs',
     opts = {
@@ -369,8 +366,8 @@ require('lazy').setup({
             dotnet_suppress_inlay_hints_for_parameters_that_match_argument_name = true,
             dotnet_suppress_inlay_hints_for_parameters_that_match_method_intent = true,
           },
-          ['csharp|code_lens'] = {
-            dotnet_enable_references_code_lens = true,
+          ['csharp|symbol_search'] = {
+            dotnet_search_reference_assemblies = false,
           },
         },
       },
@@ -563,6 +560,16 @@ require('lazy').setup({
       } } },
       'williamboman/mason-lspconfig.nvim',
       'WhoIsSethDaniel/mason-tool-installer.nvim',
+      -- {
+      --   'seblj/roslyn.nvim',
+      --   ft = 'cs',
+      --   opts = {
+      --     exe = {
+      --       'dotnet',
+      --       '/home/jdavis/.local/share/nvim/mason/packages/roslyn/libexec/Microsoft.CodeAnalysis.LanguageServer.dll',
+      --     },
+      --   },
+      -- },
 
       -- Useful status updates for LSP.
       { 'j-hui/fidget.nvim', opts = {} },
@@ -731,6 +738,27 @@ require('lazy').setup({
         -- But for many setups, the LSP (`ts_ls`) will work just fine
         -- ts_ls = {},
         --
+        -- roslyn = {
+        --   settings = {
+        --     ['csharp|inlay_hints'] = {
+        --       csharp_enable_inlay_hints_for_implicit_object_creation = true,
+        --       csharp_enable_inlay_hints_for_implicit_variable_types = true,
+        --       csharp_enable_inlay_hints_for_lambda_parameter_types = true,
+        --       csharp_enable_inlay_hints_for_types = true,
+        --       dotnet_enable_inlay_hints_for_indexer_parameters = true,
+        --       dotnet_enable_inlay_hints_for_literal_parameters = true,
+        --       dotnet_enable_inlay_hints_for_object_creation_parameters = true,
+        --       dotnet_enable_inlay_hints_for_other_parameters = true,
+        --       dotnet_enable_inlay_hints_for_parameters = true,
+        --       dotnet_suppress_inlay_hints_for_parameters_that_differ_only_by_suffix = true,
+        --       dotnet_suppress_inlay_hints_for_parameters_that_match_argument_name = true,
+        --       dotnet_suppress_inlay_hints_for_parameters_that_match_method_intent = true,
+        --     },
+        --     ['csharp|symbol_search'] = {
+        --       dotnet_search_reference_assemblies = true,
+        --     },
+        --   },
+        -- },
         zls = {
           settings = {
             zls = {
@@ -804,6 +832,8 @@ require('lazy').setup({
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
       require('mason-lspconfig').setup {
+        ensure_installed = {},
+        automatic_installation = true,
         handlers = {
           function(server_name)
             local server = servers[server_name] or {}
@@ -977,21 +1007,31 @@ require('lazy').setup({
     end,
   },
 
-  { -- You can easily change to a different colorscheme.
-    -- Change the name of the colorscheme plugin below, and then
-    -- change the command in the config to whatever the name of that colorscheme is.
-    --
-    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'folke/tokyonight.nvim',
-    priority = 1000, -- Make sure to load this before all the other start plugins.
-    init = function()
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+  -- { -- You can easily change to a different colorscheme.
+  -- Change the name of the colorscheme plugin below, and then
+  -- change the command in the config to whatever the name of that colorscheme is.
+  --
+  -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
+  --'folke/tokyonight.nvim',
+  --priority = 1000, -- Make sure to load this before all the other start plugins.
+  --init = function()
+  -- Load the colorscheme here.
+  -- Like many other themes, this one has different styles, and you could load
+  -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+  --vim.cmd.colorscheme 'tokyonight-night'
 
-      -- You can configure highlights by doing something like:
-      vim.cmd.hi 'Comment gui=none'
+  -- You can configure highlights by doing something like:
+  --vim.cmd.hi 'Comment gui=none'
+  --end,
+  --},
+  {
+    'navarasu/onedark.nvim',
+    priority = 1000,
+    opts = {
+      style = 'warmer',
+    },
+    init = function()
+      vim.cmd.colorscheme 'onedark'
     end,
   },
 
@@ -1071,11 +1111,11 @@ require('lazy').setup({
   --  Uncomment any of the lines below to enable them (you will need to restart nvim).
   --
   -- require 'kickstart.plugins.debug',
-  -- require 'kickstart.plugins.indent_line',
+  require 'kickstart.plugins.indent_line',
   -- require 'kickstart.plugins.lint',
-  -- require 'kickstart.plugins.autopairs',
+  require 'kickstart.plugins.autopairs',
   -- require 'kickstart.plugins.neo-tree',
-  -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
+  require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
