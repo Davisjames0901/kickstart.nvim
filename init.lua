5003b35ee33063b931ed085ec9849865f0a3c12e
asperand <jamesdavis0901@gmail.com>
2025-07-07 11:47:04 -0400

    updating

--- init.lua
+++ init.lua
@@ -761,13 +761,14 @@ require('lazy').setup({
         -- },
         zls = {
           settings = {
-            zls = {
-              enable_inlay_hints = true,
-              inlay_hints_show_builtin = true,
-              inlay_hints_exclude_single_argument = true,
-              inlay_hints_hide_redundant_param_names = false,
-              inlay_hints_hide_redundant_param_names_last_token = false,
-            },
+            enable_inlay_hints = true,
+            inlay_hints_show_builtin = true,
+            inlay_hints_exclude_single_argument = false,
+            enable_build_on_save = true,
+            inlay_hints_hide_redundant_param_names = true,
+            inlay_hints_hide_redundant_param_names_last_token = true,
+            zig_exe_path = '/home/jdavis/.zvm/bin/zig',
+            zig_lib_path = '/home/jdavis/.zvm/bin/lib',
           },
         },
         --omnisharp = {
